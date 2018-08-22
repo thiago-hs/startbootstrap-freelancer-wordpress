e23116bc95f4f1acf5306d942f0095f5c5dde7a6
sor_thiago <sor_thiago@hotmail.com>
2018-08-22 19:07:47 -0400

    putting the theme name in a variable

--- functions.php
+++ functions.php
@@ -2,25 +2,27 @@
 
 function add_theme_scripts() {
   
+  $tpl_directory = get_template_directory_uri() ;
+  
   //font-awesome
-  wp_enqueue_style( 'font-awesome',get_template_directory_uri()  . '/vendor/font-awesome/css/font-awesome.min.css',array(),'4.7.0','all' );
+  wp_enqueue_style( 'font-awesome',$tpl_directory  . '/vendor/font-awesome/css/font-awesome.min.css',array(),'4.7.0','all' );
 
   //bootstrap
-  wp_enqueue_style( 'bootstrap-css',get_template_directory_uri()  . '/vendor/bootstrap/css/bootstrap.min.css',array(),'4.1.1','all' );
-  wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.bundle.min.js', array ( 'jquery' ), '4.1.1', true);
+  wp_enqueue_style( 'bootstrap-css',$tpl_directory . '/vendor/bootstrap/css/bootstrap.min.css',array(),'4.1.1','all' );
+  wp_enqueue_script( 'bootstrap-js', $tpl_directory. '/vendor/bootstrap/js/bootstrap.bundle.min.js', array ( 'jquery' ), '4.1.1', true);
 
   //jquery-easing
-  wp_enqueue_script( 'jquery-easing-js', get_template_directory_uri() . '/vendor/jquery-easing/jquery.easing.min.js', array ( 'jquery' ), '1.4.1', true);
+  wp_enqueue_script( 'jquery-easing-js', $tpl_directory . '/vendor/jquery-easing/jquery.easing.min.js', array ( 'jquery' ), '1.4.1', true);
 
   //magnific-popup
-  wp_enqueue_style( 'magnific-popup-css',get_template_directory_uri()  . '/vendor/magnific-popup/magnific-popup.css',array(),'1.1.0 ','all' );
-  wp_enqueue_script( 'magnific-popup-js', get_template_directory_uri() . '/vendor/magnific-popup/jquery.magnific-popup.min.js', array ( 'jquery' ), '1.1.0 ', true);
+  wp_enqueue_style( 'magnific-popup-css',$tpl_directory  . '/vendor/magnific-popup/magnific-popup.css',array(),'1.1.0 ','all' );
+  wp_enqueue_script( 'magnific-popup-js', $tpl_directory . '/vendor/magnific-popup/jquery.magnific-popup.min.js', array ( 'jquery' ), '1.1.0 ', true);
 
-  wp_enqueue_script('jqBootstrapValidation', get_template_directory_uri() . '/js/jqBootstrapValidation.js', array ( 'jquery' ), 1.0 , true);
-  wp_enqueue_script('contact_me', get_template_directory_uri() . '/js/contact_me.js', array ( 'jquery' ), 1.0 , true);
+  wp_enqueue_script('jqBootstrapValidation', $tpl_directory . '/js/jqBootstrapValidation.js', array ( 'jquery' ), 1.0 , true);
+  wp_enqueue_script('contact_me', $tpl_directory . '/js/contact_me.js', array ( 'jquery' ), 1.0 , true);
 
-  wp_enqueue_style('freelancer-css', get_template_directory_uri() . '/css/freelancer.min.css', false, '1.0.0', 'all');
-  wp_enqueue_script('freelancer-js', get_template_directory_uri() . '/js/freelancer.min.js', array ( 'jquery' ), 1.0 , true);
+  wp_enqueue_style('freelancer-css', $tpl_directory . '/css/freelancer.min.css', false, '1.0.0', 'all');
+  wp_enqueue_script('freelancer-js', $tpl_directory . '/js/freelancer.min.js', array ( 'jquery' ), 1.0 , true);
   
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
